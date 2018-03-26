52ce4129c06a2a8d04c765c4dde92fa84cc6a5f9
Edgar Hernandez <edgarchortiz@gmail.com>
2018-03-26 11:16:35 -0700

    Create Restaurants Detail Activity

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantDetailActivity.java
+++
@@ -1,8 +0,0 @@
-package com.epicodus.myrestaurants.ui;
-
-/**
- * Created by Edgar on 3/26/2018.
- */
-
-public class RestaurantDetailActivity {
-}
---
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantsDetailActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.myrestaurants.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants_detail);
+    }
+}
